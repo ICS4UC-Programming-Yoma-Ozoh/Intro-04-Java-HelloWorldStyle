37387dfbcef64c23f48b8340cb06d03e458e17ca
Yoma Ozoh <ogeneyoma.ozoh@stu.ocsb.ca>
2026-09-14 13:23:22 +0000

    hellowrld

---
+++ HelloWorldStyle.java
@@ -0,0 +1,6 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, Planet Earth!");
+         System.out.println("Have a great day today");
+    }
+}
